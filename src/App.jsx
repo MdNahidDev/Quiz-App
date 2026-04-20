4a5a18da1eec3a937b97f162d823c4fe749d9249
MdNahidDev <mdnahidhasan.tech@gmail.com>
2026-04-20 18:03:30 +0600

    3rd

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,29 @@ const quizPromise = fetch("/quizes.json").then((res) => res.json());
 function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [score, setScore] = useState(0);
-  const [selected, setSelected] = useState();
-  const [userAnswer, setUserAnswer];
+  const [selected, setSelected] = useState("");
+  const [userAnswer, setUserAnswer] = useState([]);
+  console.log(score);
+  const hanldleNext = (currentQuiz) => {
+    const isCorrect = selected === currentQuiz.answer;
+    if (isCorrect) setScore(score + 1);
+    setUserAnswer([...userAnswer, selected]);
+    setCurrentIndex(currentIndex + 1);
+    setSelected("");
+  };
 
   return (
     <div className="min-h-screen bg-base-300 flex items-center justify-center p-4">
       <Suspense
         fallback={<span className="loading loading-spinner loading-lg"></span>}
       >
-        <Questions quizPromise={quizPromise} />
+        <Questions
+          currentIndex={currentIndex}
+          hanldleNext={hanldleNext}
+          selected={selected}
+          setSelected={setSelected}
+          quizPromise={quizPromise}
+        />
       </Suspense>
     </div>
   );
--- src/Components/Questions/Questions.jsx
+++ src/Components/Questions/Questions.jsx
@@ -1,9 +1,14 @@
 import React, { use } from "react";
 
-const Questions = ({ quizPromise }) => {
+const Questions = ({
+  quizPromise,
+  selected,
+  setSelected,
+  hanldleNext,
+  currentIndex,
+}) => {
   const quizData = use(quizPromise);
-  console.log(quizData);
-  const currentQuiz = quizData[0];
+  const currentQuiz = quizData[currentIndex];
   return (
     <div>
       <h2 className="text-2xl font-semibold my-8 leading-tight">
@@ -13,13 +18,18 @@ const Questions = ({ quizPromise }) => {
         {currentQuiz.options.map((option) => (
           <button
             key={option}
-            className="btn justify-start text-left text-[15px] h-auto py-4 px-6"
+            className={`btn justify-start text-left text-[15px] h-auto py-4 px-6 ${selected === option ? "btn-primary" : "btn-outline"}`}
+            onClick={() => setSelected(option)}
           >
             {option}
           </button>
         ))}
       </div>
-      <button className="btn btn-primary btn-block text-base h-auto py-4 px-6 mt-8">
+      <button
+        className="btn btn-primary btn-block text-base h-auto py-4 px-6 mt-8"
+        onClick={() => hanldleNext(currentQuiz)}
+        disabled={!selected}
+      >
         Finish Quiz
       </button>
     </div>
